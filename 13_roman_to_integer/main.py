2618c74cff6568b68f2bec2aadc5f9da98dced24
Maliqi Mario <maliqimario@gmail.com>
2023-10-13 00:21:14 +0200

    use more string.replace()!

--- 13_roman_to_integer/main.py
+++ 13_roman_to_integer/main.py
@@ -2,30 +2,23 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romanValue = { 
+        translations = {
             "I": 1,
             "V": 5,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 1000,
+            "M": 1000
         }
-
-        num = 0
-        for i in range(len(s)):
-            if i != len(s) - 1:
-                if s[i] == 'I' and s[i + 1] == 'V' or s[i + 1] == 'X':
-                    num += s[i + 1] - 1
-                elif s[i] == 'X' and s[i + 1] == 'L' or s[i + 1] == 'C':
-                    num += s[i + 1] - 10
-                elif s[i] == 'C' and s[i + 1] == 'D' or s[i + 1] == 'M':
-                    num += s[i + 1] - 100
-                else:
-                    num += romanValue[s[i]]
-            else:
-                num += romanValue[s[i]]
-        return num
+        number = 0
+        # just string.replace duh!
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
 if __name__ == "__main__":
     res = Solution().romanToInt("MCMXCIV")
